5c425c40dcde9f8ce4d4a80872f93a4f2c6ad51d
JCashen <JCashen@qa.com>
2020-12-04 23:31:00 +0000

    update

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
 echo $PATH
-pip3 install -r assesment2/requirements.txt
-python3 -m pytest assesment2/service1 --cov assesment2/service1
-python3 -m pytest assesment2/service2 --cov assesment2/service2
-python3 -m pytest assesment2/service3 --cov assesment2/service3
-python3 -m pytest assesment2/service4 --cov assesment2/service4
+pip3 install -r requirements.txt
+python3 -m pytest service1 --cov service1
+python3 -m pytest service2 --cov service2
+python3 -m pytest service3 --cov service3
+python3 -m pytest service4 --cov service4
