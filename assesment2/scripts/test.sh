3bd824a64b520b2b555d232000bd8fc8ac868d95
JCashen <JCashen@qa.com>
2020-12-04 10:13:08 +0000

    update

--- assesment2/scripts/test.sh
+++ assesment2/scripts/test.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 pip3 install pytest pytest-cov
-pytest assesment2/service1 --cov ./service1
-pytest assesment2/service2 --cov ./service2
-pytest assesment2/service3 --cov ./service3
-pytest assesment2/service4 --cov ./service4
+python3 -m pytest assesment2/service1 --cov ./service1
+python3 -m pytest assesment2/service2 --cov ./service2
+python3 -m pytest assesment2/service3 --cov ./service3
+python3 -m pytest assesment2/service4 --cov ./service4
