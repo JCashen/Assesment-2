0ea30a5c56ad21ea7dd848e35b5861d2f5f80035
JCashen <JCashen@qa.com>
2020-12-04 23:48:40 +0000

    update

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
 ssh swarm-manager << EOF
-docker pull jcashen/service1:latest
-docker pull jcashen/service2:latest
-docker pull jcashen/service3:latest
-docker pull jcashen/service4:latest
+sudo docker pull jcashen/service1:latest
+sudo docker pull jcashen/service2:latest
+sudo docker pull jcashen/service3:latest
+sudo docker pull jcashen/service4:latest
 git clone https://github.com/JCashen/Assesment-2.git
 cd Assesment-2/
-docker stack deploy --compose-file docker-compose.yaml gacha_app
+sudo docker stack deploy --compose-file docker-compose.yaml gacha_app
 EOF
