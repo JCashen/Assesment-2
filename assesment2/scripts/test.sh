1fb160326ad6211356d2633ea2056f62cd688f81
JCashen <JCashen@qa.com>
2020-12-04 10:04:22 +0000

    update

--- assesment2/scripts/test.sh
+++ assesment2/scripts/test.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-pip3 install -r requirements.txt
+pip3 install -r assesment2/requirements.txt
 pytest ./service1 --cov ./service1
 pytest ./service2 --cov ./service2
 pytest ./service3 --cov ./service3
