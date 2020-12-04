470369a7708b37390454c47897b1e251212fd1a0
JCashen <JCashen@qa.com>
2020-12-04 23:44:58 +0000

    update

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,11 @@
 #! /bin/bash
 
-cd assesment2
+ssh jenkins@swarm-master << EOF
+docker pull jcashen/service1:latest
+docker pull jcashen/service2:latest
+docker pull jcashen/service3:latest
+docker pull jcashen/service4:latest
+git clone https://github.com/JCashen/Assesment-2.git
+cd Assesment-2/
 docker stack deploy --compose-file docker-compose.yaml gacha_app
+EOF
