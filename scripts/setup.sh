d2d5f61bf226eb4fe02d0558a0a1bc52fcf3963b
JCashen <JCashen@qa.com>
2020-12-06 13:18:09 +0000

    update

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
+git clone https://github.com/JCashen/Assesment-2.git
 cd Assesment-2
 echo $PATH
 sudo /home/jenkins/.local/bin/ansible-playbook -i ansible/inventory ansible/playbook.yaml
